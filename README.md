9038b73797dd2734dcac8da7f52a4852b39f0318
bachsofttrick <xuanbach1307@gmail.com>
2026-01-03 22:27:38 -0800

    Add llama-cpp-python and start testing

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # OCR AI Web
 A web application that utilize LightOnOCR model to transcribe image to text.
+This application uses components licensed under the Apache License, Version 2.0.  
 
 ## Todo
 - [x] Create a Docker container to work in.
 - [x] Get llama-cpp-python to work
 - [x] Get GPU to work in it. llama.cpp doesn't need CUDA so Vulkan is fine
-- [ ] Get llama-cpp-python to use noctrex/LightOnOCR-1B-1025-GGUF model
-  - [ ] Test an image on it and see response
+- [!] Get llama-cpp-python to use noctrex/LightOnOCR-1B-1025-GGUF model
+  - [!] Test an image on it and see response
 - [ ] Create a fullstack (React/FastAPI) web with pages:
   - [ ] Homepage: Upload image
   - [ ] After upload, maybe store in cache and tell user to confirm the image to be uploaded
@@ -14,4 +15,5 @@ A web application that utilize LightOnOCR model to transcribe image to text.
   - [ ] LightOnOCR returns result, give it to user. Find ways to prevent rambling (or it cannot read the image)
   - [ ] Dry run on your PC
 
-This application uses components licensed under the Apache License, Version 2.0.  
+llama-cpp-python gets stuck easily, doesn't work as well as sending JSON to OpenAI API of llama.cpp.
+That works much better. And this was on Qwen2-VL model.
---
+++ llmtest.py
@@ -0,0 +1,59 @@
+from llama_cpp import Llama
+from llama_cpp.llama_chat_format import Llava15ChatHandler
+import base64
+import json
+import os
+
+def jpg_to_base64(image_path):
+    """
+    Converts a JPG image file to a Base64 encoded string.
+    """
+    if not os.path.exists(image_path):
+        raise Exception(f"Error: File not found at {image_path}")
+
+    # Open the image file in binary read mode ('rb')
+    with open(image_path, "rb") as image_file:
+        # Read the file's binary content and encode it to Base64
+        encoded_bytes = base64.b64encode(image_file.read())
+        # Decode the bytes to a standard UTF-8 string
+        base64_string = encoded_bytes.decode("utf-8")
+        return base64_string
+
+os.system('clear')
+
+# Import mmproj for vision part of the multimodal
+chat_handler = Llava15ChatHandler(
+    clip_model_path='/app/models/Qwen2.5-VL-3B-Instruct-mmproj-F16.gguf'
+)
+
+llm = Llama(
+    model_path='/app/models/Qwen2.5-VL-3B-Instruct-Q4_K_M.gguf',
+    n_gpu_layers=99,
+    n_ctx=0,
+    chat_handler=chat_handler,
+    verbose=False
+)
+
+dataimage = 'data:image/jpeg;base64,' + jpg_to_base64('/app/motivational-quote-for-inspirataion.jpg')
+
+output = llm.create_chat_completion(
+    messages=[
+        {
+            "role": "system",
+            "content": "Extract text from image"
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": dataimage
+                    }
+                }
+            ]
+        }
+    ]
+)
+
+print(json.dumps(output, indent=2))
